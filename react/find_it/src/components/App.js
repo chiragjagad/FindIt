ef22d4b708b3ed0441d7249b8a6e312393137cc8
nimai <nimai.m.patel@gmail.com>
2021-03-13 13:33:39 +0530

    added top navbar

--- react/find_it/src/components/App.js
+++ react/find_it/src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import Sidebar from '../components/Sidebar/Sidebar';
+import TopNavbar from '../components/TopNavbar/TopNavbar'
+
 function App() {
   return (
     <div className="App">
