fa20d5a74986b6b582f52325d6f09bc0ec8429cf
nimai <nimai.m.patel@gmail.com>
2021-03-14 00:36:21 +0530

    real time chat feature done

--- react/find_it/src/pages/SingleChat.js
+++ react/find_it/src/pages/SingleChat.js
@@ -6,19 +6,35 @@ import AllChats from './AllChats'
 
 const SingleChat = ({ match }) => {
 
+    const otherUid = match.params.chatId
+
     const uid = "DWctr1yJVoaV46SKQxQ5"
 
-    const query = firestoreDB.collection(`chats/${uid}/messages`)
-    const [messages] = useCollectionData(query)
+    const [ textField, setTextField ] = useState("")
+
+    const chatsRef = firestoreDB.collection('chats')
+    const interactions = chatsRef.where('receiver', 'in', [uid, otherUid], 'AND', 'sender', 'in', [uid, otherUid])
+    const [messages] = useCollectionData(interactions)
+
+    const sendMessage = () => {
+        firestoreDB.collection('chats').add({
+            sender: uid,
+            receiver: otherUid,
+            message: textField,
+            createdAt: Firebase.firestore.FieldValue.serverTimestamp()
+        })
+    }
 
     return (
         <div style={{ width: '100%' }} >
             {messages && messages.map(message => <div style={{ 
                 display: 'flex',
                 margin: '10px',
-                justifyContent: uid === message.idUser ? 'flex-end' : 'flex-start',
-                backgroundColor: uid === message.idUser ? 'red' : 'blue',
-            }}><p>{message.idUser}</p></div>)}
+                justifyContent: uid === message.sender ? 'flex-end' : 'flex-start',
+                backgroundColor: uid === message.sender ? 'red' : 'blue',
+            }}><p>{message.message}</p></div>)}
+        <input value={textField} onChange={event => setTextField(event.target.value)}/>
+        <button onClick={sendMessage}>GO</button>
         </div>
     )
 }
