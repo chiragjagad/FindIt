78295369532fbd25265c98f5ada933e0158c04c9
Chirag Jagad <chiragjagad08@gmail.com>
2021-03-13 13:36:09 +0530

    Sidebar and content

--- react/find_it/src/components/App.js
+++ react/find_it/src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "../components/Sidebar/Sidebar";
+import Cards from "./Cards/Cards";
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <Cards />
     </div>
   );
 }
