a7848354d22ae90763fc84500c694b7d0746bbfb
Shaurya Magar <shaurya.magar1@gmail.com>
2021-03-13 18:05:00 +0530

    login/register redirect to dashboard

--- react/find_it/src/pages/Dashboard.js
+++ react/find_it/src/pages/Dashboard.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Cards from "../components/Cards/Cards";
 import Sidebar from "../components/Sidebar/Sidebar";
+import firebase from 'firebase';
+import {useHistory} from 'react-router-dom';
 import TopNavbar from "../components/TopNavbar/TopNavbar";
 const Dashboard=()=>{
-    
+  const history = useHistory();
+  useEffect(()=>{
+    var user = firebase.auth().currentUser;
+    if(!user){
+      history.push("/login");
+    }
+  })
   return (
     <div className="App">
       <Sidebar />
