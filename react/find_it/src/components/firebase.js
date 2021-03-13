0786e14215b4818dbb803b76398563d9fe001170
Shaurya Magar <shaurya.magar1@gmail.com>
2021-03-13 12:39:13 +0530

    added firebase config file, made directories for pages and components

---
+++ react/find_it/src/components/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+          apiKey: "AIzaSyBM0xrLUZ3RACucl2XGMleG15XaimG-WzM",
+          authDomain: "find-it-fe243.firebaseapp.com",
+          projectId: "find-it-fe243",
+          storageBucket: "find-it-fe243.appspot.com",
+          messagingSenderId: "97059543954",
+          appId: "1:97059543954:web:b7153976dcba672d7b7f4b"
+  });
+
+const db = firebaseApp.database();
+
+export { db };
+
