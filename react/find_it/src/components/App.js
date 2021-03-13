4d3be4447183142214358937e6a2cfcffec4fdc3
Chirag Jagad <chiragjagad08@gmail.com>
2021-03-13 18:39:20 +0530

    conflict resolved

--- react/find_it/src/components/App.js
+++ react/find_it/src/components/App.js
@@ -10,34 +10,18 @@ import AllChats from "../pages/AllChats";
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Router>
         <Switch>
           <div className="App">
             <Route exact path="/register" component={Register}></Route>
             <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/chat" component={AllChats} />
+            <Route exact path="/chats" component={AllChats} />
             <Route path="/chat/:chatId" component={SingleChat} />
             <Route exact path="/login" component={Login} />
           </div>
         </Switch>
       </Router>
     </>
-=======
-			<Router>
-				<Switch>
-					<div className="App">
-						<Route exact path="/register" component={Register}></Route>
-						<Route exact path="/dashboard" component={Dashboard} />
-                        <Route exact path="/chats" component={AllChats}/>
-                        <Route path="/chat/:chatId" component={SingleChat}/>
-						<Route exact path="/login" component={Login} />
-					</div>
-				</Switch>
-			</Router>
-		</>
-
->>>>>>> f57c7f989519251e9ddf7a61dc4a966444642cd3
   );
 }
 
