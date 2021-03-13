45afb4e8c1c93342ff47cea9ff118ba6e16b049a
Shaurya Magar <shaurya.magar1@gmail.com>
2021-03-13 15:02:14 +0530

    email auth functional

--- react/find_it/src/components/App.js
+++ react/find_it/src/components/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Dashboard from '../pages/Dashboard';
+import Register from '../pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard/>
-    </div>
+    <>
+			<Router>
+				<Switch>
+					<div className="App">
+						<Route exact path="/register" component={Register}></Route>
+						<Route exact path="/dashboard" component={Dashboard} />
+					</div>
+				</Switch>
+			</Router>
+		</>
 
   );
 }
--- react/find_it/src/pages/Register.js
+++ react/find_it/src/pages/Register.js
@@ -1,6 +1,35 @@
 import React from 'react';
-
+import firebase from "firebase";
+import { db } from "../components/firebase";
+import { Redirect, useHistory,useLocation } from "react-router-dom";
 const Register = ()=>{
+    const [name,setName]=React.useState("");
+    const [email,setEmail]=React.useState("");
+    const [pass,setPass]=React.useState("");
+    const [rep, setRep] = React.useState("");
+    const onFormSubmit=(e)=>{
+        e.preventDefault();
+        if(pass!==rep||pass===""){
+            alert("Entered passwords don't match");
+        }else{
+            firebase.auth().createUserWithEmailAndPassword(email, pass)
+                .then((userCredential) => {
+                    // Signed in 
+                    var user = userCredential.user;
+                    console.log(user);
+
+                    // ...
+                })
+                .catch((error) => {
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    console.log(errorCode);
+                    // ..
+                });
+        }
+        
+    }
+    
     return(
     <>
     <div class="container">
@@ -14,41 +43,32 @@ const Register = ()=>{
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                             </div>
-                            <form class="user">
+                            <form class="user" onSubmit={onFormSubmit}>
                                 <div class="form-group row">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
                                         <input type="text" class="form-control form-control-user" id="exampleFirstName"
-                                            placeholder="First Name"/>
+                                            placeholder="Name of the Organization" value={name} onChange={e=>setName(e.target.value)}/>
                                     </div>
-                                    <div class="col-sm-6">
-                                        <input type="text" class="form-control form-control-user" id="exampleLastName"
-                                            placeholder="Last Name"/>
                                     </div>
-                                </div>
                                 <div class="form-group">
                                     <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                                        placeholder="Email Address"/>
+                                        placeholder="Email Address" value={email} onChange={e=>setEmail(e.target.value)}/>
                                 </div>
                                 <div class="form-group row">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
                                         <input type="password" class="form-control form-control-user"
-                                            id="exampleInputPassword" placeholder="Password"/>
+                                            id="exampleInputPassword" placeholder="Password" onChange={e=>setPass(e.target.value)} value={pass}/>
                                     </div>
                                     <div class="col-sm-6">
                                         <input type="password" class="form-control form-control-user"
-                                            id="exampleRepeatPassword" placeholder="Repeat Password"/>
+                                            id="exampleRepeatPassword" placeholder="Repeat Password" value={rep} onChange={e=>setRep(e.target.value)}/>
                                     </div>
                                 </div>
-                                <a href="login.html" class="btn btn-primary btn-user btn-block">
+                                <button type="submit" class="btn btn-primary btn-user btn-block">
                                     Register Account
-                                </a>
-                                <hr/>
-                                <a href="index.html" class="btn btn-google btn-user btn-block">
-                                    <i class="fab fa-google fa-fw"></i> Register with Google
-                                </a>
-                                <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                    <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
-                                </a>
+                                </button>
+                               
+                                
                             </form>
                             <hr/>
                             <div class="text-center">
